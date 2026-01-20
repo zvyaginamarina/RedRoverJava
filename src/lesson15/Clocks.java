8a8500221a7a7074003149478b9d8784aefd0f7f
mzviagina <mzviagina@custis.ru>
2026-01-20 11:15:47 +0700

    Auto commit

--- src/lesson15/Clocks.java
+++ src/lesson15/Clocks.java
@@ -2,42 +2,37 @@ package lesson15;
 
 public class Clocks {
 
-    public static void showTime(){
-        // 
-        
+    public static void showTime() {
+        //
+
         int hour = 0;
-        OUTTER:
-        while (hour<=6){
-            int minute = 0; 
-            MIDDLE:
-            do {
+        OUTTER: while (hour <= 6) {
+            int minute = 0;
+            MIDDLE: do {
                 int second = 0;
 
-                if(hour>1 && minute % 10 == 0){
+                if (hour > 1 && minute % 10 == 0) {
                     break OUTTER;
                 }
 
-                INNER:
-                while (second <60){
-                    System.out.println(hour+":"+minute+":"+second);
+                INNER: while (second < 60) {
+                    System.out.println(hour + ":" + minute + ":" + second);
 
-                    if (second*hour > minute){
+                    if (second * hour > minute) {
                         continue MIDDLE;
                     }
                     second++;
                 }
                 minute++;
-                
-            } while (minute<=59);
+
+            } while (minute <= 59);
 
             hour++;
 
         }
     }
-        
 
     public static void main(String[] args) {
         showTime();
     }
 }
-
