b6c7f1ce1ef3a89adfb7803b1a282a8d3b3508f1
mzviagina <mzviagina@custis.ru>
2026-01-20 17:38:03 +0700

    Auto commit

--- src/lesson16/UserEmail2.java
+++ src/lesson16/UserEmail2.java
@@ -2,9 +2,14 @@ package lesson16;
 
 public class UserEmail2 {
 
-    String s = "";
+    String emails = "ya@yahoo.com; on@mail.ru; ona@gmail.ru";
 
     public void email(String s) {
+        char c1;
+
+        for (int i = 0; i < s.length(); i++){
+            c1 = 
+        }
 
     }
 
