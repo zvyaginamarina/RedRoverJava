144861ec79cb7a3fef9efe27e21cb5788ccc71ac
mzviagina <mzviagina@custis.ru>
2026-01-20 15:03:11 +0700

    Auto commit

--- src/lesson16/Test3.java
+++ src/lesson16/Test3.java
@@ -6,6 +6,12 @@ public class Test3 {
         System.out.println(s1.isBlank());
         String s2 = "     ";
         System.out.println(s2.isBlank());
+        String s3 = " ";
+        System.out.println(s3.isEmpty());
+        String s4 = "";
+        System.out.println(s4.isEmpty());
+        String s5 = null;
+        System.out.println(s5.isEmpty());
     }
 
 }
