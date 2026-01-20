3c2e4f761c0f0333e2c615d70428d8a88194933a
mzviagina <mzviagina@custis.ru>
2026-01-20 17:31:32 +0700

    Auto commit

--- src/lesson16/UserEmail.java
+++ src/lesson16/UserEmail.java
@@ -14,8 +14,8 @@ public class UserEmail {
             } else if (c1 == '.') {
                 int i2 = i;
                 String s2 = s.substring(i1, i2);
-                System.out.println();
                 System.out.print(s2);
+                System.out.println();
             }
         }
     }
