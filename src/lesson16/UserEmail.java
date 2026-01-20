0f55292d9d910a63595b78b4da36918d42239734
mzviagina <mzviagina@custis.ru>
2026-01-20 16:59:14 +0700

    Auto commit

--- src/lesson16/UserEmail.java
+++ src/lesson16/UserEmail.java
@@ -3,13 +3,15 @@ package lesson16;
 public class UserEmail {
 
     char c1;
-    char c2;
+    int i1;
 
     public void email(String s) {
         for (int i = 0; i < s.length(); i++) {
             c1 = s.charAt(i);
             if (c1 == '@') {
+                i1 = s.indexOf(i);
                 System.out.println();
+                System.out.println(c1);
             }
 
         }
