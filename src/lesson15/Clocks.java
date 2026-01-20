e37976dc2408d464b89a1b984bb14cc76685f515
mzviagina <mzviagina@custis.ru>
2026-01-20 11:10:02 +0700

    Auto commit

--- src/lesson15/Clocks.java
+++ src/lesson15/Clocks.java
@@ -9,16 +9,21 @@ public class Clocks {
         OUTTER:
         while (hour<=6){
             int minute = 0; 
+            MIDDLE:
             do {
                 int second = 0;
+
+                if()
+                INNER:
                 while (second <60){
                     System.out.println(hour+":"+minute+":"+second);
 
                     if (second*hour > minute){
-                        
+                        continue MIDDLE;
                     }
                     second++;
                 }
+
                 
             } while (true);
 
