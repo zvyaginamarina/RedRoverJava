f7c0acf00ea73c2d276f4fa7d9db2bd1f9a0c967
mzviagina <mzviagina@custis.ru>
2026-01-20 12:15:23 +0700

    Auto commit

--- src/lesson16.java
+++ src/lesson16.java
@@ -1,3 +1,16 @@
 public class lesson16 {
 
+    public static void main(String[] args) {
+        String s1 = new String("Hello");
+        String s2 = new String("Hello");
+
+        System.out.println(s1 == s2);
+
+        String s3 = "Bye";
+        String s4 = "Bye";
+
+        System.out.println(s3 == s4);
+
+    }
+
 }
