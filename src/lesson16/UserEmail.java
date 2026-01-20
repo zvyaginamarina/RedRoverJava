5c991c2c1495d0f48e58f43014853ab4a465bd20
mzviagina <mzviagina@custis.ru>
2026-01-20 17:16:36 +0700

    Auto commit

--- src/lesson16/UserEmail.java
+++ src/lesson16/UserEmail.java
@@ -4,16 +4,19 @@ public class UserEmail {
 
     char c1;
     int i1;
+    int i2;
 
     public void email(String s) {
         for (int i = 0; i < s.length(); i++) {
             c1 = s.charAt(i);
 
             if (c1 == '@') {
-                String s2 = s.substring(i + 1);
-                System.out.println(s2);
+                i1 = i + 1;
+            } else if (c == '.') {
+                i2 = i;
             }
-            // System.out.print(c1);
+            String s2 = s.substring(i1, i2);
+            System.out.print(s2);
         }
 
     }
