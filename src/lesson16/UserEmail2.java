b0f1e9a8cb5d390320eb033ba6f51c76567f9ed1
mzviagina <mzviagina@custis.ru>
2026-01-20 17:46:48 +0700

    Auto commit

--- src/lesson16/UserEmail2.java
+++ src/lesson16/UserEmail2.java
@@ -5,22 +5,41 @@ public class UserEmail2 {
     String emails = "ya@yahoo.com; on@mail.ru; ona@gmail.ru";
 
     public void email(String s) {
-        char c1;
+        // char c1;
 
-        for (int i = 0; i < s.length(); i++) {
-            c1 = s.charAt(i);
-            int i1;
+        // for (int i = 0; i < s.length(); i++) {
+        // c1 = s.charAt(i);
+        // int i1;
 
-            if (c1 == '@') {
-                i1 = i + 1;
+        // if (c1 == '@') {
+        // i1 = i + 1;
+        // }
+
+        // if (c1 != '@' && c1 != '.' && c1 != ';') {
+
+        // }
+
+        // }
+
+        int a = 0; // индекс символа @
+        int b = 0; // индекс символа .
+        int c = 0; // индекс символа ;
+
+        while (c < s.length() - 1) {
+            a = s.indexOf('@', c);
+            b = s.indexOf('.', a);
+            c = s.indexOf(';', b);
+
+            if (c == -1) { // Если это последний email в списке (после него нет ;)
+                c = s.length();
             }
 
-            if (c1 != '@' && c1 != '.' && c1 != ';') {
-
-            }
-
+            System.out.println(s.substring(a + 1, b));
         }
+    }
 
+    public static void main(String[] args) {
+        email(emails);
     }
 
 }
