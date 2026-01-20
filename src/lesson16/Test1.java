0a0630aded8a46d571fe25ddf01b4520d7fdf604
mzviagina <mzviagina@custis.ru>
2026-01-20 12:49:59 +0700

    Auto commit

--- src/lesson16/Test1.java
+++ src/lesson16/Test1.java
@@ -2,4 +2,12 @@ package lesson16;
 
 public class Test1 {
 
+    public static void main(String[] args) {
+        String s1 = new String("Hello!");
+
+        int a = s1.length();
+        System.out.println(a);
+
+    }
+
 }
