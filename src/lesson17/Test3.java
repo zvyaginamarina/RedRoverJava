e2f77e4234a5d384c6c989da775c6eda5d72ed9f
mzviagina <mzviagina@custis.ru>
2026-01-21 17:50:08 +0700

    Auto commit

--- src/lesson17/Test3.java
+++ src/lesson17/Test3.java
@@ -2,8 +2,14 @@ package lesson17;
 
 public class Test3 {
     public static void main(String[] args) {
-        StringBuilder sb1 = new StringBuilder();
-        String s = 
+        StringBuilder sb1 = new StringBuilder("123");
+        String s1 = new String(sb1);
+        System.out.println(s1);
+
+        StringBuffer sb2 = new StringBuffer("456");
+        String s2 = new String(sb2);
+
+        System.out.println(s2);
     }
 
 }
